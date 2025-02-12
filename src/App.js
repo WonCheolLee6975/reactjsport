676b17abf330f83e89f3499134977444f9fc1d57
DESKTOP-6JI48IR\Won <cjf2210@gmail.com>
2025-02-12 16:22:47 +0900

    컴퍼넌트 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom;"
+
+import HomeView from "./view/HomeView";
 
 const App = () => {
-    return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
