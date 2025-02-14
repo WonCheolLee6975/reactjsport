28b6c5c1f135776cf28b628e59566ca9a1019e0a
DESKTOP-6JI48IR\Won <cjf2210@gmail.com>
2025-02-14 10:33:22 +0900

    인트로 완성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom;"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import HomeView from "./view/HomeView";
+import HomeView from "./views/HomeView";
 
 const App = () => {
   return (
